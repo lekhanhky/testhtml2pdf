f1029f854ab9821eec1cf80b0f88050c3c78aa15
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-15 08:43:10 +0000

    cập nhật

--- lib/custom_code/actions/new_custom_action.dart
+++ lib/custom_code/actions/new_custom_action.dart
@@ -9,19 +9,23 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:pdf/widgets.dart' as pw;
+import 'package:permission_handler/permission_handler.dart';
 
 Future newCustomAction() async {
   // Add your function code here!
-  final pdf = pw.Document();
+  var status = await Permission.storage.status;
+  if (!status.isGranted) {
+    final pdf = pw.Document();
 
-  pdf.addPage(
-    pw.Page(
-      build: (pw.Context context) => pw.Center(
-        child: pw.Text('Hello World!'),
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Center(
+          child: pw.Text('Hello World!'),
+        ),
       ),
-    ),
-  );
+    );
 
-  final file = File('example.pdf');
-  await file.writeAsBytes(await pdf.save());
+    final file = File('example.pdf');
+    await file.writeAsBytes(await pdf.save());
+  }
 }
